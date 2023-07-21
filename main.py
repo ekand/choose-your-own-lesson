79432e7d8de8513bd2a37e374f0575456b227e75
Erik Anderson <erik.kristofer.anderson@gmail.com>
2023-07-21 13:31:39 -0500

    fix mistake

--- main.py
+++ main.py
@@ -14,14 +14,15 @@ first_page = Page(str(uuid.uuid4()), 'First Lesson', 'first', 'https://youtu.be/
 pages = [first_page, what_is_this, done_page]
 
 for page in pages:
-    with open('pelican/content/' + page.slug + '.md', 'w') as out_file:
-        video_id = page.video_url.split('/')[-1]
-        youtube_s = f'<iframe width="560" height="315" src="https://www.youtube.com/embed/{video_id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
-        s = f'Title: {page.title}\nDate: 2023-07-21\n Category: Lesson\nslug: {page.slug}\n\n' \
-            f'###Watch this video:  ' \
-            f'{youtube_s}  \n'
-        if page.next_lesson is not None:
-            s += f'###' + page.next_lesson.question_text + '\n\n'
+    video_id = page.video_url.split('/')[-1]
+    youtube_s = f'<iframe width="560" height="315" src="https://www.youtube.com/embed/{video_id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
+    s = f'Title: {page.title}\nDate: 2023-07-21\nCategory: Lesson\nslug: {page.slug}\n\n' \
+        f'###Watch this video:  ' \
+        f'{youtube_s}  \n'
+    if page.next_lesson is not None:
+        s += f'###' + page.next_lesson.question_text + '\n\n'
 
-            for answer in page.next_lesson.list_of_answers:
-                s += f'{answer.answer_text}    [Go here]({list(filter(lambda p: p.id == answer.id_of_page_with_answer, pages))[0].slug}.html)\n\n'
+        for answer in page.next_lesson.list_of_answers:
+            s += f'{answer.answer_text}    [Go here]({list(filter(lambda p: p.id == answer.id_of_page_with_answer, pages))[0].slug}.html)\n\n'
+    with open('pelican/content/' + page.slug + '.md', 'w') as out_file:
+        out_file.write(s)
