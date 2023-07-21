3129bbc9f2d1cc914e287acd2eb175164c400be4
Erik Anderson <erik.kristofer.anderson@gmail.com>
2023-07-21 13:20:31 -0500

    draft of learn python by making music

--- main.py
+++ main.py
@@ -3,22 +3,49 @@ import uuid
 from my_classes import Page, NextLessonStructure, Answer
 
 done_page = Page(str(uuid.uuid4()), "You are done.", 'done', 'https://youtu.be/6xi0Uw9XU_U', 'Done video', None)
+
 what_is_this = Page(str(uuid.uuid4()), "What is this??", 'what', 'https://youtu.be/guHNT6lAa5I', 'What is this video',
                     NextLessonStructure("Do you have any questions?",
                                         [Answer("I have no questions", done_page.id)]))
 
-first_page = Page(str(uuid.uuid4()), 'First Lesson', 'first', 'https://youtu.be/80tv4j052H4', 'First video',
+template_page = Page(str(uuid.uuid4()), 'Lesson Title', 'first', 'https://youtu.be/<id>', '',
+                     NextLessonStructure("Do you have any questions?",
+                                         [Answer("I have no questions", done_page.id),
+                                          Answer("<question_text>?", what_is_this.id),
+                                          Answer("<question_text>?", what_is_this.id)]))
+
+what_does_equal_do = Page(str(uuid.uuid4()), 'What Does The Equal Sign Do?', 'what-does-equal-do',
+                          'https://youtu.be/<id>', '',
+                          NextLessonStructure("Do you have any questions?",
+                                              [Answer("I have no questions", done_page.id),
+                                               Answer("What is this?", what_is_this.id)]))
+
+why_quotes = Page(str(uuid.uuid4()), 'Why Is Hello World Wrapped In Quotes?', 'why-quotes', 'https://youtu.be/<id>', '',
                   NextLessonStructure("Do you have any questions?",
                                       [Answer("I have no questions", done_page.id),
-                                       Answer("What is this?", what_is_this.id)]))
-pages = [first_page, what_is_this, done_page]
+                                       Answer("What does the equal sign do?", what_does_equal_do.id)]))
+
+first_page = Page(str(uuid.uuid4()), 'Learn Python By Making Music: First Lesson', 'first',
+                  'https://youtu.be/80tv4j052H4', 'First video',
+                  NextLessonStructure("Do you have any questions?",
+                                      [Answer("I have no questions", done_page.id),
+                                       Answer("What does the equal sign do?", what_does_equal_do.id),
+                                       Answer("Why is Hello World wrapped in quotes?", what_is_this.id),
+                                       ]))
+
+pages = [first_page, why_quotes, what_does_equal_do, what_is_this, done_page]
+for page in pages:
+    if page.video_title == '':
+        page.video_title = page.title
 
 for page in pages:
     with open('pelican/content/' + page.slug + '.md', 'w') as out_file:
-
+        video_id = page.video_url.split('/')[-1]
+        youtube_s = f'<iframe width="560" height="315" src="https://www.youtube.com/embed/{video_id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
         s = f'Title: {page.title}\nDate: 2023-07-21\n Category: Lesson\nslug: {page.slug}\n\n' \
-            f'{page.title}\n\n' \
-            f'###Watch this video then come back: [{page.video_title}]({page.video_url})\n\n'
+            f'###Watch this video: ' \
+            f'{youtube_s}  \n'
+            #f'[{page.video_title}]({page.video_url})\n\n'
         if page.next_lesson is not None:
             s += f'###' + page.next_lesson.question_text + '\n\n'
 
--- pelican/content/first.md
+++ pelican/content/first.md
@@ -1,15 +1,14 @@
-Title: First Lesson
+Title: Learn Python By Making Music: First Lesson
 Date: 2023-07-21
  Category: Lesson
 slug: first
 
-First Lesson
-
-###Watch this video then come back: [First video](https://youtu.be/80tv4j052H4)
-
+###Watch this video: <iframe width="560" height="315" src="https://www.youtube.com/embed/80tv4j052H4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>  
 ###Do you have any questions?
 
 I have no questions    [Go here](done.html)
 
-What is this?    [Go here](what.html)
+What does the equal sign do?    [Go here](what-does-equal-do.html)
+
+Why is Hello World wrapped in quotes?    [Go here](what.html)
 
